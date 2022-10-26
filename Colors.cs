53a5b1fdf54252552486c1c65c84db925ed37ce3
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-10-26 10:37:22 -0600

    colors wtih errrors

---
+++ Colors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game{
+
+    public class Color{
+
+        private int red = 0;
+        private int green = 0;
+        private int blue = 0;
+        private int alpha = 225;
+
+        public Color(int red, int green, int blue)
+        {
+            int.red = red; 
+            int.green = green;
+            int.blue = blue;
+        }
+        
+        public int GetAlpha(){
+            return alpha;
+        }
+
+        public int GetBlue(){
+            return blue;
+        }
+        public int GetRed(){
+            return red;
+        }
+        public int GetGreen(){
+            return green;
+        }
+    }
+
+}
