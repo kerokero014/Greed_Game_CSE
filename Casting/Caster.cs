709bc1018355f4e339ea12e12553bc4cc40804b1
kerokero014 <101151724+kerokero014@users.noreply.github.com>
2022-11-01 14:30:04 -0600

    Program.cs main prgram

--- Casting/Caster.cs
+++ Casting/Caster.cs
@@ -46,7 +46,6 @@ namespace Game.Casting{
 
             if (actors.ContainsKey(group))
             {
-
                 if (actors[group].Count > 0)
                 {    
                     result = actors[group][0];
